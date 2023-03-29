5b133718ab08922405bcb114b11e9ea0fac15158
unknown <ganeshyadavstudies@gmail.com>
2023-03-29 23:41:39 +0530

    added header and footer component

--- hiker/src/App.js
+++ hiker/src/App.js
@@ -1,11 +1,12 @@
 import Header from './components/header';
 import './App.css';
-
+import  Footer from "./components/footer"
 
 function App() {
   return (
     <div className="App">
      <Header/>
+     <Footer/>
     </div>
   );
 }
